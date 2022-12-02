ddc92a7df41b7f2efe6605e57c10412cfb94b138
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-12-02 03:23:49 -0800

    bug fixed, 1 bloc not free

--- tokenizer.c
+++ tokenizer.c
@@ -5,13 +5,15 @@ char **tokenizer(char *cmd);
 char **tokenizer(char *cmd)//ls -la
 {
 	unsigned int i = 0;
-	char *copy_cmd;
+	char *copy_cmd1;
+	char *copy_cmd2;
 	char *token;
 	char **argv = NULL;
 	int argc = 0;
 
-	copy_cmd = strdup(cmd);
-	token = strtok(copy_cmd, " ");
+	copy_cmd1 = strdup(cmd);
+	copy_cmd2 = strdup(cmd);
+	token = strtok(copy_cmd1, " ");
 
 	while (token)
 	{
@@ -20,34 +22,35 @@ char **tokenizer(char *cmd)//ls -la
 		argc++;
 	}
 
-	argv = malloc(sizeof(char *) * argc);
+	argv = malloc(sizeof(char *) * (argc + 1));
 
-	token = strtok(copy_cmd, " ");
+	token = strtok(copy_cmd2, " ");
 	while (token)
 	{
 		argv[i] = token;
 		printf("tokenizer print: %s\n", argv[i]);
-		i++;
 		token = strtok(NULL, " \n");
+		i++;
 	}
 	argv[i] = NULL;
-	
+	free(copy_cmd1);
 	return (argv);
 }
 
 int main(void)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	char *cmd = "jean louis";
 	char **argv = NULL;
 
 	//argv = malloc(sizeof(char *) * 3);
 	argv = tokenizer(cmd);
 	
-	while (argv[i] != NULL)
+	while (argv[i])
 	{
 		printf("%s\n", argv[i]);
 		i++;
 	}
+	free(argv);
 	return (0);
 }
